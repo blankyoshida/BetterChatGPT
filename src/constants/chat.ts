c83bc3e161ea8429feb21d597e9389de3d598d35
kilnen-lab <149597753+kilnen-lab@users.noreply.github.com>
2023-12-04 00:31:44 +0900

    change defaultModel

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -13,9 +13,7 @@ const dateString =
 // default system message obtained using the following method: https://twitter.com/DeminDimin/status/1619935545144279040
 export const _defaultSystemMessage =
   import.meta.env.VITE_DEFAULT_SYSTEM_MESSAGE ??
-  `You are ChatGPT, a large language model trained by OpenAI.
-Carefully heed the user's instructions. 
-Respond using Markdown.`;
+  `You are ChatGPT, a large language model trained by OpenAI.Carefully heed the user's instructions.Respond using Markdown.Use Japanese unless otherwise instructed.`;
 
 export const modelOptions: ModelOptions[] = [
   //'gpt-3.5-turbo',
@@ -29,7 +27,7 @@ export const modelOptions: ModelOptions[] = [
   // 'gpt-4-32k-0314',
 ];
 
-export const defaultModel = 'gpt-3.5-turbo';
+export const defaultModel = 'gpt-4-1106-preview';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
