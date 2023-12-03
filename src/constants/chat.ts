c67f3b184456cce45e78fe5f0b12db93b8eaa2b2
kilnen-lab <149597753+kilnen-lab@users.noreply.github.com>
2023-12-04 00:39:20 +0900

    fix modelOptions

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -16,11 +16,11 @@ export const _defaultSystemMessage =
   `You are ChatGPT, a large language model trained by OpenAI.Carefully heed the user's instructions.Respond using Markdown.Use Japanese unless otherwise instructed.`;
 
 export const modelOptions: ModelOptions[] = [
-  //'gpt-3.5-turbo',
-  //'gpt-3.5-turbo-16k',
+  'gpt-3.5-turbo',
+  'gpt-3.5-turbo-16k',
   'gpt-3.5-turbo-1106',
-  //'gpt-4',
-  //'gpt-4-32k',
+  'gpt-4',
+  'gpt-4-32k',
   'gpt-4-1106-preview'
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
