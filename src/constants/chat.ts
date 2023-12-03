9ebc8fede6d4e79211b77e354ac32c5bda0201a1
kilnen-lab <149597753+kilnen-lab@users.noreply.github.com>
2023-12-04 01:22:04 +0900

    test maxtoken

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -27,7 +27,7 @@ export const modelOptions: ModelOptions[] = [
   // 'gpt-4-32k-0314',
 ];
 
-export const defaultModel = 'gpt-4-1106-preview';
+export const defaultModel = 'gpt-3.5-turbo-1106';
 
 export const modelMaxToken = {
   //'gpt-3.5-turbo': 4096,
@@ -35,7 +35,7 @@ export const modelMaxToken = {
   //'gpt-3.5-turbo-0613': 4096,
   //'gpt-3.5-turbo-16k': 16384,
   //'gpt-3.5-turbo-16k-0613': 16384,
-  'gpt-3.5-turbo-1106': 16384,
+  'gpt-3.5-turbo-1106': 100,
   //'gpt-4': 8192,
   //'gpt-4-0314': 8192,
   //'gpt-4-0613': 8192,
